66f90644d3fd933b5fd1fc3e6f2cd7b9bb801f31
Salma <salma.ashraf@appnest.pro>
2026-09-10 13:33:48 +0300

    (feat):delete quiz endpoint

---
+++ Features/Quizzes/AdminDeleteQuiz/Controllers/DeleteQuizController.cs
@@ -0,0 +1,26 @@
+﻿using exam_system.Features.Quizzes.AdminDeleteQuiz.Commands;
+using exam_system.Features.Quizzes.AdminUpdateQuiz.Commands;
+using exam_system.Features.Quizzes.AdminUpdateQuiz.Dtos;
+using exam_system.Features.Shared;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace exam_system.Features.Quizzes.AdminDeleteQuiz.Controllers;
+
+[ApiController]
+[Route("api/quizzes")]
+public class DeleteQuizController(IMediator mediator) : ControllerBase
+{
+    [HttpDelete("{id:guid}")]
+    public async Task<IActionResult> Delete(
+        Guid id,
+        CancellationToken cancellationToken)
+    {
+        var result = await mediator.Send(new DeleteQuizCommand(id), cancellationToken);
+
+        return StatusCode(
+            result.StatusCode,
+            EndpointResponse.FromResult(result)
+        );
+    }
+}
